fb733b002b0cc59a90934c3638b2f4c9d3654ebd
kaajc <jgress1802@gmail.com>
2022-10-18 19:04:16 -0500

    More grinding

--- HuffmanEncode.java
+++ HuffmanEncode.java
@@ -1,16 +1,44 @@
 import java.io.*;
 import java.util.*;
 public class HuffmanEncode {
+    
+    public String fileIn;
+    public String fileOut;
+    public BufferedReader br;
+
     public HuffmanEncode(String in, String out) {
         //Implements the main flow of your program 
         //in is the name of the source file 
         //out is the name of the output file
         //Add private methods and instance variables as needed
+
+        fileIn = in;
+        fileOut = out;
+
+
+        try{
+            FileReader fr = new FileReader(in);
+            br = new BufferedReader(fr);
+            PriorityQueue<Character> queue = createPriorityQueue(br);
+
+
+        }catch(FileNotFoundException e){
+            System.out.println("File not found");
         }
+    }
+
+    public PriorityQueue<Character> createPriorityQueue(BufferedReader br){
+        
+    }
+
+    
+
+
+
     public static void main(String args[]) {
         //args[0] is the name of the source file 
         //args[1] is the name of the output file
-        new HuffmanEncode(args[0], args[1]));
+        new HuffmanEncode(args[0], args[1]);
         //do not change anything here
     }
 }
--- Test.java
+++ Test.java
@@ -1,6 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.PriorityQueue;
+
 public class Test {
-   public static void main(String[] args){
-      int[] x = new int[120];
-      System.out.println(x[0]);
+   public static void main(String[] args) throws IOException{
+      PriorityQueue<Character> x = new PriorityQueue<Character>();
+      FileReader fr = new FileReader("./test.txt");
+      BufferedReader br = new BufferedReader(fr);
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.read());
+      System.out.println(br.readLine());
    } 
 }
